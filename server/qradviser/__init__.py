3ee84207e0a87cea6ffddc43e76f7335bdb62c0e
Maruo.S <raspi-maru2004@outlook.jp>
2024-10-15 23:08:52 +0900

    second sec fix

--- server/qradviser/__init__.py
+++ server/qradviser/__init__.py
@@ -47,9 +47,9 @@ if settings.SERVE_STATIC is not None:
     @app.get('/{full_path:path}')
     async def spa(full_path:str):
         if full_path=="": full_path="index.html"
-        full_path = os.path.normpath(f"{settings.SERVE_STATIC}/" + full_path)
+        full_path = os.path.normpath(f"{settings.SERVE_STATIC}/{full_path}")
         if not full_path.startswith(settings.SERVE_STATIC):
-            raise HTTPException(status_code=403, detail="Forbidden")
+            raise Forbidden(status_code=403, detail="Forbidden")
         if os.path.exists(full_path):
             return FileResponse(full_path)
         else:
