c4bdce8c2903066e44fe782edd69ffad39a7de2d
Jenova3300 <ykonpaku00@gmail.com>
2024-11-08 21:02:09 +0900

    fix: icontainsを使用していたのをconstainsに変更

--- server/qradviser/routes/search.py
+++ server/qradviser/routes/search.py
@@ -7,5 +7,5 @@ router = APIRouter(tags=["Search"])
 
 @router.get("/{search_word}", response_model=List[StopData])
 async def search(search_word: str):
-    stops = await Stop.filter(name__icontains=search_word)
+    stops = await Stop.filter(name__contains=search_word)
     return [StopData(stop_id=stop.id, name=stop.name) for stop in stops]
