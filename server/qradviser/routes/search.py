58f8330551fc706e93e07cc85d7b9786b4043870
Jenova3300 <ykonpaku00@gmail.com>
2024-11-08 21:04:11 +0900

    fix: search_wordsをパスパラメータでなくクエリパラメータで取るように変更

--- server/qradviser/routes/search.py
+++ server/qradviser/routes/search.py
@@ -5,7 +5,7 @@ from typing import List
 
 router = APIRouter(tags=["Search"])
 
-@router.get("/{search_word}", response_model=List[StopData])
+@router.get("/", response_model=List[StopData])
 async def search(search_word: str):
     stops = await Stop.filter(name__contains=search_word)
     return [StopData(stop_id=stop.id, name=stop.name) for stop in stops]
