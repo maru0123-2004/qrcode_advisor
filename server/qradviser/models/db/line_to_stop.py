37ade2396213e4673e76084f3deb0061ad7b060e
Maruo.S <raspi-maru2004@outlook.jp>
2024-11-16 18:03:19 +0900

    rename column due to bug in column insertion

--- server/qradviser/models/db/line_to_stop.py
+++ server/qradviser/models/db/line_to_stop.py
@@ -2,8 +2,8 @@ from tortoise import Model
 from tortoise.fields import ForeignKeyField, IntField
 
 class LineStop(Model):
-    stop_id = ForeignKeyField("models.Stop", related_name="stops")
-    line_id = ForeignKeyField("models.Line", related_name="lines")
+    stop = ForeignKeyField("models.Stop", related_name="stops")
+    line = ForeignKeyField("models.Line", related_name="lines")
     order = IntField()
     class Meta:
         table = 'linestop'
